53d55af42b0e99d48c84959af5a21a48ab29050d
roman <roma.sluka.97@mail.ru>
2018-03-09 23:50:30 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,75 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.result = "" + initialValue;
+        this.numberForPow = [];
+        this.prePowOperation = "+";
+        this.unPrePowOperation = "-";
+        this.prePowNumber = initialValue;
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    forPowFunction() {
+        if (this.numberForPow.length != 0) {
+            while (this.numberForPow.length != 1) {
 
-  devide(number) {
-    // your implementation
-  }
+                this.numberForPow[this.numberForPow.length - 2] = Math.pow(this.numberForPow[this.numberForPow.length - 2], this.numberForPow.pop())
 
-  pow(number) {
-    // your implementation
-  }
+            }
+            this.result += `${this.unPrePowOperation} ${this.prePowNumber} ${this.prePowOperation}  Math.pow(${this.prePowNumber}, ${this.numberForPow.pop()})`;
+        }
+    }
+
+    add(number) {
+        this.forPowFunction();
+
+        this.result += `+${number}`;
+        this.prePowNumber = number;
+        this.prePowOperation = "+";
+        this.unPrePowOperation = "-";
+        return this;
+    }
+
+    subtract(number) {
+        this.forPowFunction();
+        this.result += `-${number}`;
+        this.prePowNumber = number;
+        this.prePowOperation = "-";
+        this.unPrePowOperation = "+";
+        return this;
+    }
+
+    multiply(number) {
+        this.forPowFunction();
+
+        this.result += `*${number}`;
+        this.prePowNumber = number;
+        this.prePowOperation = "*";
+        this.unPrePowOperation = "/";
+        return this;
+    }
+
+    devide(number) {
+        this.forPowFunction();
+
+        this.result += `/${number}`;
+        this.prePowNumber = number;
+        this.prePowOperation = "/";
+        this.unPrePowOperation = "*";
+
+        return this;
+    }
+
+    pow(number) {
+        this.numberForPow.push(number);
+        return this;
+    }
+    toString() {
+        this.forPowFunction();
+        return eval(this.result);
+    }
+
+
 }
+
 
 module.exports = SmartCalculator;
